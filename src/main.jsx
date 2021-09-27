b52ec03e5f7d7983550f2ef16a1b272c4e2fc978
Ali Shah <s.ashah@outlook.com>
2021-09-27 04:19:12 -0400

    [Enabled] Redux devtools extension

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,12 @@ import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
 
 ReactDOM.render(
   <React.StrictMode>

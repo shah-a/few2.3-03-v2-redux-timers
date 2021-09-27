004c1b3e6d6b88a599acc012bcebac127561eb8e
Ali Shah <s.ashah@outlook.com>
2021-09-27 07:07:20 -0400

    [Update] Timer component displays formatted time

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import './Timer.css';
 import { toggleTimer } from '../../actions';
+import { formatTime } from '../../utils';
 
 function Timer({ timer, index }) {
   const dispatch = useDispatch();
@@ -8,7 +9,7 @@ function Timer({ timer, index }) {
   return (
     <div className="Timer">
       <h2>{timer.name}</h2>
-      <h1>{timer.time}</h1>
+      <h1>{formatTime(timer.time)}</h1>
       <button
         type="button"
         onClick={() => dispatch(toggleTimer(index))}
